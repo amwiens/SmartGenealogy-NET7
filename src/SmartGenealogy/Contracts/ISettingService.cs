3617adc2a9c5862f337dcc1f48184056bdfcad23
Aaron Wiens <aaron@awwebsolutions.com>
2023-07-16 22:36:31 -0500

    #2 Created the Avalonia project with logging and window settings service.

---
+++ src/SmartGenealogy/Contracts/ISettingService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+using SmartGenealogy.Models;
+
+namespace SmartGenealogy.Contracts;
+
+public interface ISettingService
+{
+    public Settings Settings { get; }
+
+    Task InitializeSettings();
+}
---
+++ src/SmartGenealogy/Models/Settings.cs
@@ -0,0 +1,42 @@
+﻿namespace SmartGenealogy.Models;
+
+public class Settings
+{
+    #region Main window settings
+
+    /// <summary>
+    /// Width of the main window.
+    /// </summary>
+    public double Width { get; set; }
+
+    /// <summary>
+    /// Height of the main window.
+    /// </summary>
+    public double Height { get; set; }
+
+    /// <summary>
+    /// Position X of the main window.
+    /// </summary>
+    public int X { get; set; }
+
+    /// <summary>
+    /// Position Y of the main window.
+    /// </summary>
+    public int Y { get; set; }
+
+    /// <summary>
+    /// Is the main window maximized.
+    /// </summary>
+    public bool IsMaximized { get; set; }
+
+    #endregion Main window settings
+
+    #region Clone
+
+    public Settings Clone()
+    {
+        return (Settings)MemberwiseClone();
+    }
+
+    #endregion
+}
