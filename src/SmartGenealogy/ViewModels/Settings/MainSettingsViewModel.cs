54ede12fef2fbdd431035db71ddc25f0d3ca6a48
Aaron Wiens <aaron@awwebsolutions.com>
2023-07-18 19:23:21 -0500

    #11 Added tab view to the settings page.

--- src/SmartGenealogy/ViewModels/Settings/MainSettingsViewModel.cs
+++ src/SmartGenealogy/ViewModels/Settings/MainSettingsViewModel.cs
@@ -1,17 +1,26 @@
-﻿using Avalonia;
+﻿using System.Collections.ObjectModel;
+
+using Avalonia;
 
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 using Serilog;
 
 using SmartGenealogy.Contracts;
+using SmartGenealogy.Models;
+using SmartGenealogy.ViewModels.Home;
+using SmartGenealogy.ViewModels.Search;
 
 namespace SmartGenealogy.ViewModels.Settings;
 
 public partial class MainSettingsViewModel : ViewModelBase
 {
-    private readonly ILogger _logger;
-    private readonly ISettingService _settingService;
+    private readonly ILogger? _logger;
+    private readonly ISettingService? _settingService;
+
+    private readonly HomeViewModel? _homeViewModel;
+    private readonly MainSearchViewModel? _mainSearchViewModel;
 
     [ObservableProperty]
     private string _currentVersion = typeof(Controls.PageHeaderControl).Assembly.GetName().Version?.ToString()!;
@@ -19,10 +28,64 @@ public partial class MainSettingsViewModel : ViewModelBase
     [ObservableProperty]
     private string _currentAvaloniaVersion = typeof(Application).Assembly.GetName().Version?.ToString()!;
 
-    public MainSettingsViewModel(ILogger logger,
-        ISettingService settingService)
+    [ObservableProperty]
+    private ViewModelBase? _currentTab;
+
+    [ObservableProperty]
+    private TabItem _selectedTab;
+
+    [ObservableProperty]
+    private ObservableCollection<TabItem> _tabItems = new();
+
+    /// <summary>
+    /// Ctor
+    /// </summary>
+    public MainSettingsViewModel() : this(null, null, null, null)
+    {
+    }
+
+    /// <summary>
+    /// Ctor
+    /// </summary>
+    public MainSettingsViewModel(ILogger? logger,
+        ISettingService? settingService,
+        HomeViewModel? homeViewModel,
+        MainSearchViewModel? mainSearchViewModel)
     {
         _logger = logger;
         _settingService = settingService;
+
+        _homeViewModel = homeViewModel;
+        _mainSearchViewModel = mainSearchViewModel;
+
+        SetupTabs();
+        SelectedTab = TabItems[0];
+        SwitchTab();
+
+        _logger?.Information("Settings view initialized");
+    }
+
+    /// <summary>
+    /// Set up the tabs on the settings page.
+    /// </summary>
+    private void SetupTabs()
+    {
+        TabItems.Clear();
+        TabItems = new ObservableCollection<TabItem>()
+        {
+            new((ViewModelBase)_homeViewModel!, "Home", "Home"),
+            new((ViewModelBase)_mainSearchViewModel!, "Search", "Search"),
+        };
+    }
+
+    /// <summary>
+    /// Switch tabs command.
+    /// </summary>
+    [RelayCommand]
+    private void SwitchTab()
+    {
+        CurrentTab = SelectedTab.ViewModel;
+        var name = SelectedTab.Name;
+        _logger?.Information("Switching settings tab content to {Name}", name);
     }
 }
