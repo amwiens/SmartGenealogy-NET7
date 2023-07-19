3f24a0965b619956746b9042dc9808dd8db8ffe9
Aaron Wiens <aaron@awwebsolutions.com>
2023-07-18 22:57:43 -0500

    #14 Added Open file code.

---
+++ src/SmartGenealogy/Messages/OpenFileChangedMessage.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+
+namespace SmartGenealogy.Messages;
+
+/// <summary>
+/// Open file changed message.
+/// </summary>
+public class OpenFileChangedMessage : ValueChangedMessage<bool>
+{
+    /// <summary>
+    /// Ctor
+    /// </summary>
+    public OpenFileChangedMessage(bool fileOpen)
+        : base(fileOpen)
+    { }
+}
